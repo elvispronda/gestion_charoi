bfbc5008302205fb60b7e777dab285815039145d
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-22 12:10:57 -0700

    Auto Commit - 2025-04-22 12:10:56

--- app/models.py
+++ app/models.py
@@ -5,14 +5,6 @@ from sqlalchemy.sql.expression import text
 from .database import Base
 
 
-class Post(Base):
-    __tablename__ = "posts"
-    id = Column(Integer, primary_key = True)
-    title = Column(String, nullable = False)
-    content = Column(String, nullable = False)
-    published = Column(Boolean,server_default = 'TRUE', nullable = False)
-    created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'))
-    owner_id  = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),nullable = False)
 #########################################################################################################################################
 
 class User(Base):
