6ffd65f96d8b5b37f668fb0b2f53ce7cba536cf5
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 16:59:11 -0700

    Auto Commit - 2025-04-23 16:59:11

--- app/routers/category_maintenance.py
+++ app/routers/category_maintenance.py
@@ -56,17 +56,17 @@ def delete_cat_maintenance(id:int,db:Session = Depends(get_db), current_user : s
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 ############################################################################################################################
 
-@router.put("/{id}", response_model=schemas.UserCreate)
-def update_user(id:int,updated_user:schemas.UserCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.CategoryMaintenanceCreate)
+def update_cat_maintenance(id:int,updated_user:schemas.CategoryMaintenanceCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
     
   
-    user_query = db.query(models.User).filter(models.User.id == id)
-    user =user_query.first()
-    if user == None:
+    cat_maintenance_query = db.query(models.CategoryMaintenance).filter(models.CategoryMaintenance.id == id)
+    cat_maintenance =cat_maintenance_query.first()
+    if cat_maintenance == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id: {id} does not exist")
    
-    user_query.update(updated_user.dict(),synchronize_session = False)
+    cat_maintenance_query.update(updated_user.dict(),synchronize_session = False)
     db.commit()
-    return user_query.first()  
+    return cat_maintenance_query.first()  
 ############################################################################################################################
