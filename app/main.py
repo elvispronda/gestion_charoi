bc6e458012765cb5496338a10b52989ca8600583
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-22 23:25:56 -0700

    Auto Commit - 2025-04-22 23:25:55

--- app/main.py
+++ app/main.py
@@ -12,8 +12,6 @@ models.Base.metadata.create_all(bind = engine)
 app = FastAPI(debug=True)        
 
     
-  
-app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
