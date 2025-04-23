c92b322d52656051125f98a2153f13e1a0b74bf7
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 02:30:03 -0700

    Auto Commit - 2025-04-23 02:30:03

--- app/schemas.py
+++ app/schemas.py
@@ -1,50 +1,44 @@
 from pydantic import BaseModel, EmailStr
-from pydantic.types import conint
-from datetime import datetime
 from typing import Optional, Union
-from datetime import date
+from datetime import datetime
 
 ###################################################################################################################
 class UserBase(BaseModel):
-    username :str
-    full_name : str
-    email : EmailStr
-    password : str
-    role : str
-
+    username: str
+    full_name: str
+    email: EmailStr
+    password: str
+    role: str
 
 class UserOut(UserBase):
-    id : int
-    email : EmailStr
-    created_at : datetime
-    
-    class config :
-        from_attributes = True   
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
 
 class UserCreate(UserBase):
     pass
-   
 
 class UserLogin(BaseModel):
-    email : EmailStr
-    password : str
-    
+    email: EmailStr
+    password: str
+
 class Token(BaseModel):
-    access_token : str
-    token_type : str
-    
+    access_token: str
+    token_type: str
 
 class TokenData(BaseModel):
-    id: Union[str, int]  # Accepts both string and integer types
-#################################################################################################################
+    id: Union[str, int]
+###################################################################################################################
 
 class DriverBase(BaseModel):
     nom: str
     prenom: str
     cni: str
     email: str
-    matricule:str
-   
+    matricule: str
+
 class BudgetCreate(DriverBase):
     pass
 
@@ -58,7 +52,6 @@ class BudgetOut(DriverBase):
 
 class CategoryFuelBase(BaseModel):
     fuel_name: str
-    
 
 class CategoryFuelCreate(CategoryFuelBase):
     pass
@@ -69,13 +62,12 @@ class CategoryFuelOut(CategoryFuelBase):
 
     class Config:
         from_attributes = True
-
-#################################################################################################################
+##################################################################################################################
 
 class FuelBase(BaseModel):
     vehicle_id: int
     fuel_type_id: int
-    quantity:float
+    quantity: float
     cost: float
 
 class FuelCreate(FuelBase):
@@ -87,7 +79,7 @@ class FuelOut(FuelBase):
 
     class Config:
         from_attributes = True
-#################################################################################################################
+##################################################################################################################
 
 class TripBase(BaseModel):
     origin: str
@@ -105,30 +97,10 @@ class TripOut(TripBase):
 
     class Config:
         from_attributes = True
-
 ##################################################################################################################
-# class UserBase(BaseModel):
-#     username: str
-#     full_name: str
-#     email: EmailStr
 
-# class UserCreate(UserBase):
-#     password: str
-
-# class UserOut(UserBase):
-#     id: int
-#     role: str
-
-#     class Config:
-#         orm_mode = True
-
-# class UserLogin(BaseModel):
-#     username: str
-#     password: str
-
-###################################################################################################################
 class VehicleTransmissionBase(BaseModel):
-    vehicle_transmission:str
+    vehicle_transmission: str
 
 class VehicleTransmissionCreate(VehicleTransmissionBase):
     pass
@@ -141,7 +113,7 @@ class VehicleTransmissionOut(VehicleTransmissionBase):
 ##################################################################################################################
 
 class VehicleFuelTypeBase(BaseModel):
-   fuel_type:str
+    fuel_type: str
 
 class VehicleFuelTypeCreate(VehicleFuelTypeBase):
     pass
@@ -154,7 +126,7 @@ class VehicleFuelTypeOut(VehicleFuelTypeBase):
 ##################################################################################################################
 
 class VehicleTypeBase(BaseModel):
-   vehicle_type:str
+    vehicle_type: str
 
 class VehicleTypeCreate(VehicleTypeBase):
     pass
@@ -164,10 +136,10 @@ class VehicleTypeOut(VehicleTypeBase):
 
     class Config:
         from_attributes = True
-###################################################################################################################
+##################################################################################################################
 
 class VehicleMakeBase(BaseModel):
-    vehicle_make:str
+    vehicle_make: str
 
 class VehicleMakeCreate(VehicleMakeBase):
     pass
@@ -177,20 +149,20 @@ class VehicleMakeOut(VehicleMakeBase):
 
     class Config:
         from_attributes = True
-###################################################################################################################
+##################################################################################################################
 
 class VehicleModelBase(BaseModel):
-   vehicle_model:str
+    vehicle_model: str
 
-class VehicleModelCreate():
+class VehicleModelCreate(VehicleModelBase):
     pass
 
-class VehicleModelOut():
+class VehicleModelOut(VehicleModelBase):
     id: int
 
     class Config:
         from_attributes = True
-####################################################################################################################
+##################################################################################################################
 
 class VehicleBase(BaseModel):
     make: int
@@ -199,13 +171,13 @@ class VehicleBase(BaseModel):
     plate_number: str
     mileage: float = 0.0
     engine_size: float
-    vehicle_type:int
-    vehicle_transmission:int
-    vehicle_fuel_type:int
-    vin:str
-    color:str
-    purchase_price:float
-    purchase_date:datetime
+    vehicle_type: int
+    vehicle_transmission: int
+    vehicle_fuel_type: int
+    vin: str
+    color: str
+    purchase_price: float
+    purchase_date: datetime
     status: str = "available"
 
 class VehicleCreate(VehicleBase):
@@ -213,16 +185,16 @@ class VehicleCreate(VehicleBase):
 
 class VehicleOut(VehicleBase):
     id: int
-    registration_date:datetime
+    registration_date: datetime
 
     class Config:
         from_attributes = True
-####################################################################################################################
+##################################################################################################################
 
 class CategoryDocumentBase(BaseModel):
-   doc_name:str
-   cost:float
-   
+    doc_name: str
+    cost: float
+
 class DocumentVehiculeCreate(CategoryDocumentBase):
     pass
 
@@ -231,27 +203,27 @@ class DocumentVehiculeOut(CategoryDocumentBase):
 
     class Config:
         from_attributes = True
-#####################################################################################################################
+##################################################################################################################
 
 class DocumentVehiculeBase(BaseModel):
-   doc_name_id: int
-   vehicle_id:int
-   issued_date:datetime
-   expiration_date:datetime
-   
+    doc_name_id: int
+    vehicle_id: int
+    issued_date: datetime
+    expiration_date: datetime
+
 class DocumentVehiculeCreate(DocumentVehiculeBase):
     pass
 
 class DocumentVehiculeOut(DocumentVehiculeBase):
     id: int
-    created_at:datetime
+    created_at: datetime
 
     class Config:
         from_attributes = True
-######################################################################################################################
+##################################################################################################################
 
 class GarageBase(BaseModel):
-   nom_garage:str
+    nom_garage: str
 
 class GarageCreate(GarageBase):
     pass
@@ -261,10 +233,10 @@ class GarageOut(GarageBase):
 
     class Config:
         from_attributes = True
-#####################################################################################################################
+##################################################################################################################
 
 class CategoryMaintenanceBase(BaseModel):
-   cat_maintenance:str
+    cat_maintenance: str
 
 class CategoryMaintenanceCreate(CategoryMaintenanceBase):
     pass
@@ -274,42 +246,29 @@ class CategoryMaintenanceOut(CategoryMaintenanceBase):
 
     class Config:
         from_attributes = True
-#####################################################################################################################
+##################################################################################################################
 
 class MaintenanceBase(BaseModel):
-   cat_maintenance_id: int
-   vehicule_id:int
-   garage_id:int
-   maintenance_cost:float
-   receipt:str
-   maintenance_date:datetime
+    cat_maintenance_id: int
+    vehicle_id: int
+    garage_id: int
+    maintenance_cost: float
+    receipt: str
+    maintenance_date: datetime
 
 class MaintenanceCreate(MaintenanceBase):
     pass
 
 class MaintenanceOut(MaintenanceBase):
     id: int
-    created_at:datetime
-
-    class Config:
-       from_attributes = True
-#####################################################################################################################
-
-class Base(BaseModel):
-   vehicle_model:str
-
-class Create():
-    pass
-
-class Out():
-    id: int
+    created_at: datetime
 
     class Config:
         from_attributes = True
-######################################################################################################################
+##################################################################################################################
 
 class CategoryPanneBase(BaseModel):
-   nom_panne: str
+    nom_panne: str
 
 class CategoryPanneCreate(CategoryPanneBase):
     pass
@@ -319,35 +278,33 @@ class CategoryPanneOut(CategoryPanneBase):
 
     class Config:
         from_attributes = True
-#######################################################################################################################
+##################################################################################################################
 
 class PanneBase(BaseModel):
-   vehicle_id:int
-   nom_panne_id:int
-   #description: str | None = None
-   description: Optional[str] = None
-   status: str
-   panne_date:datetime
-   
+    vehicle_id: int
+    nom_panne_id: int
+    description: Optional[str] = None
+    status: str
+    panne_date: datetime
+
 class PanneCreate(PanneBase):
     pass
 
 class PanneOut(PanneBase):
     id: int
-    created_at:datetime
-
+    created_at: datetime
 
     class Config:
         from_attributes = True
-########################################################################################################################
+##################################################################################################################
 
 class ReparationBase(BaseModel):
-   panne_id: int
-   cost: float
-   receipt:str
-   garage_id: int
-   repair_date:datetime
-   status:str
+    panne_id: int
+    cost: float
+    receipt: str
+    garage_id: int
+    repair_date: datetime
+    status: str
 
 class ReparationCreate(ReparationBase):
     pass
@@ -357,5 +314,5 @@ class ReparationOut(ReparationBase):
 
     class Config:
         from_attributes = True
-#########################################################################################################################
+
 
