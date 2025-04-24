e50fe90fe788081cddb60b2953e12dfbb3492714
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 18:27:07 -0700

    Auto Commit - 2025-04-23 18:27:06

--- app/routers/maintenance.py
+++ app/routers/maintenance.py
@@ -35,38 +35,38 @@ def get_maintenancelog(id : int, db :Session = Depends(get_db),  current_user : 
     maintenance = db.query(models.Maintenance).filter(models.Maintenance.id == id).first()
     
     if not maintenance :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Maintenance with this Car id : {id} was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Maintenance log with this Car id : {id} was not found")
     return maintenance
 
 #############################################################################################################################
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+def delete_maintenance(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
    
-   user_query = db.query(models.User).filter(models.User.id == id)
-   user = user_query.first()
+   maintenance_query = db.query(models.Maintenance).filter(models.Maintenance.id == id)
+   maintenance = maintenance_query.first()
    
-   if user == None:
+   if maintenance == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"user with id: {id} does not exist")
+                            detail=f"Maintenance log with this Car id: {id} does not exist")
   
          
-   user_query.delete(synchronize_session = False) 
+   maintenance_query.delete(synchronize_session = False) 
    db.commit()  
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 ############################################################################################################################
 
-@router.put("/{id}", response_model=schemas.UserCreate)
-def update_user(id:int,updated_user:schemas.UserCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.MaintenanceCreate)
+def update_maintenance(id:int,updated_maintenance:schemas.MaintenanceCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
     
   
-    user_query = db.query(models.User).filter(models.User.id == id)
-    user =user_query.first()
-    if user == None:
+    maintenance_query = db.query(models.Maintenance).filter(models.Maintenance.id == id)
+    maintenance =maintenance_query.first()
+    if maintenance == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"user with id: {id} does not exist")
+                            detail=f"Maintenance  with  car id: {id} does not exist")
    
-    user_query.update(updated_user.dict(),synchronize_session = False)
+    maintenance_query.update(updated_maintenance.dict(),synchronize_session = False)
     db.commit()
-    return user_query.first()  
+    return maintenance_query.first()  
 ############################################################################################################################
