75b7e1ce9357ab7fe1e472b6072612ef791bcc4b
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 18:13:32 -0700

    Auto Commit - 2025-04-23 18:13:32

--- app/routers/garage.py
+++ app/routers/garage.py
@@ -30,43 +30,43 @@ def get_garage(db:Session = Depends(get_db), current_user : str = Depends(oauth2
     return garages
 ############################################################################################################################
 
-@router.get("/{id}", response_model=schemas.UserOut)
-def get_user(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
-    user = db.query(models.User).filter(models.User.id == id).first()
+@router.get("/{id}", response_model=schemas.GarageOut)
+def get_garage(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
+    garage = db.query(models.Garage).filter(models.Garage.id == id).first()
     
-    if not user :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id : {id} was not found")
-    return user
+    if not garage :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Garage with id : {id} was not found")
+    return garage
 
 #############################################################################################################################
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+def delete_garage(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
    
-   user_query = db.query(models.User).filter(models.User.id == id)
-   user = user_query.first()
+   garage_query = db.query(models.Garage).filter(models.Garage.id == id)
+   garage = garage_query.first()
    
-   if user == None:
+   if garage == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"user with id: {id} does not exist")
+                            detail=f"Garage with id: {id} does not exist")
   
          
-   user_query.delete(synchronize_session = False) 
+   garage_query.delete(synchronize_session = False) 
    db.commit()  
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 ############################################################################################################################
 
-@router.put("/{id}", response_model=schemas.UserCreate)
-def update_user(id:int,updated_user:schemas.UserCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.GarageCreate)
+def update_garage(id:int,updated_garage:schemas.GarageCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
     
   
-    user_query = db.query(models.User).filter(models.User.id == id)
-    user =user_query.first()
-    if user == None:
+    garage_query = db.query(models.User).filter(models.User.id == id)
+    garage =garage_query.first()
+    if garage == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id: {id} does not exist")
    
-    user_query.update(updated_user.dict(),synchronize_session = False)
+    garage_query.update(updated_garage.dict(),synchronize_session = False)
     db.commit()
-    return user_query.first()  
+    return garage_query.first()  
 ############################################################################################################################
