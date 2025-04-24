af32c10543cb7961fa43292850384c4b71689b6a
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 17:33:31 -0700

    Auto Commit - 2025-04-23 17:33:31

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 
 from . import models 
 from .database import engine
-from .routers import  user, auth,category_document,category_maintenance,category_panne,document_vehicle
+from .routers import  user, auth,category_document,category_maintenance,category_panne,document_vehicle,drivers
 from .config import settings
 
 models.Base.metadata.create_all(bind = engine)
@@ -18,7 +18,7 @@ app.include_router(category_document.router)
 app.include_router(category_maintenance.router)
 app.include_router(category_panne.router)
 app.include_router(document_vehicle.router)
-app.include_router(user.router)
+app.include_router(drivers.router)
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(auth.router)
