9514f3a17299f9da9071cdd591b4102edead0955
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 18:50:59 -0700

    Auto Commit - 2025-04-23 18:50:59

--- app/routers/reparation.py
+++ app/routers/reparation.py
@@ -21,53 +21,53 @@ def create_reparationlog(reparation : schemas.ReparationCreate, db:Session = Dep
 
 ############################################################################################################################
 
-@router.get("/", response_model = List[schemas.UserOut])
-def get_users(db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user),
+@router.get("/", response_model = List[schemas.ReparationOut])
+def get_reparations(db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user),
               limit : int = 5, skip : int = 0, search :Optional[str] = ""):
               
   
-    ##filter all users at the same time
-    users = db.query(models.User).filter(models.User.email.contains(search)).limit(limit).offset(skip).all()
-    return users 
+    ##filter all reparations log at the same time
+    reparations = db.query(models.Reparation).filter(models.Reparation.panne_id.contains(search)).limit(limit).offset(skip).all()
+    return reparations
 ############################################################################################################################
 
-@router.get("/{id}", response_model=schemas.UserOut)
-def get_user(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
-    user = db.query(models.User).filter(models.User.id == id).first()
+@router.get("/{id}", response_model=schemas.ReparationOut)
+def get_reparationlog(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
+    reparation = db.query(models.Reparation).filter(models.Reparation.id == id).first()
     
-    if not user :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id : {id} was not found")
-    return user
+    if not reparation :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"reparation log with panne id : {id} was not found")
+    return reparation
 
 #############################################################################################################################
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+def delete_reparationlog(id:int,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
    
-   user_query = db.query(models.User).filter(models.User.id == id)
-   user = user_query.first()
+   reparation_query = db.query(models.Reparation).filter(models.Reparation.id == id)
+   reparation = reparation_query.first()
    
-   if user == None:
+   if reparation == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"user with id: {id} does not exist")
+                            detail=f"reparation log with panne id : {id} does not exist")
   
          
-   user_query.delete(synchronize_session = False) 
+   reparation_query.delete(synchronize_session = False) 
    db.commit()  
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 ############################################################################################################################
 
-@router.put("/{id}", response_model=schemas.UserCreate)
-def update_user(id:int,updated_user:schemas.UserCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.ReparationCreate)
+def update_reparation(id:int,updated_reparation:schemas.ReparationCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
     
   
-    user_query = db.query(models.User).filter(models.User.id == id)
-    user =user_query.first()
-    if user == None:
+    reparation_query = db.query(models.Reparation).filter(models.Reparation.id == id)
+    reparation =reparation_query.first()
+    if reparation == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id: {id} does not exist")
    
-    user_query.update(updated_user.dict(),synchronize_session = False)
+    reparation_query.update(updated_reparation.dict(),synchronize_session = False)
     db.commit()
-    return user_query.first()  
+    return reparation_query.first()  
 ############################################################################################################################
