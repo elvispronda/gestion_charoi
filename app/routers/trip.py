405e7b133e25018d7cea4c84c8e28c6c0e87eb94
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 19:03:04 -0700

    Auto Commit - 2025-04-23 19:03:04

--- app/routers/trip.py
+++ app/routers/trip.py
@@ -6,40 +6,37 @@ from sqlalchemy import func
 from .. import models ,schemas,oauth2,utils
 from ..database import  get_db
 
-router = APIRouter(prefix="/user", tags=['User'])
+router = APIRouter(prefix="/trip", tags=['Trip'])
 
 ############################################################################################################################
-@router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut) 
-def create_user(user : schemas.UserCreate, db:Session = Depends(get_db)):
+@router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.TripOut) 
+def create_trip(trip: schemas.TripCreate, db:Session = Depends(get_db)):
+   
     
-    # Hash the password   _ user.password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    
-    new_user = models.User(**user.dict())
-    db.add(new_user)
+    new_trip = models.User(**trip.dict())
+    db.add(new_trip)
     db.commit()
-    db.refresh(new_user)
-    return new_user
+    db.refresh(new_trip)
+    return new_trip
 
 ############################################################################################################################
 
-@router.get("/", response_model = List[schemas.UserOut])
-def get_users(db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user),
+@router.get("/", response_model = List[schemas.TripOut])
+def get_trips(db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user),
               limit : int = 5, skip : int = 0, search :Optional[str] = ""):
               
   
-    ##filter all users at the same time
-    users = db.query(models.User).filter(models.User.email.contains(search)).limit(limit).offset(skip).all()
-    return users 
+    ##filter all trips  log at the same time
+    trips = db.query(models.Trip).filter(models.Trip.vehicle_id.contains(search)).limit(limit).offset(skip).all()
+    return trips
 ############################################################################################################################
 
 @router.get("/{id}", response_model=schemas.TripOut)
-def get_trip(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
+def get_triplog(id : int, db :Session = Depends(get_db),  current_user : str = Depends(oauth2.get_current_user)):
     trip = db.query(models.Trip).filter(models.Trip.id == id).first()
     
     if not trip :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Trip with id : {id} was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Trip with this car id : {id} was not found")
     return trip
 
 #############################################################################################################################
@@ -52,7 +49,7 @@ def delete_trip(id:int,db:Session = Depends(get_db), current_user : str = Depend
    
    if trip == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Trip with id: {id} does not exist")
+                            detail=f"Trip with this car id: {id} does not exist")
   
          
    trip_query.delete(synchronize_session = False) 
@@ -68,7 +65,7 @@ def update_trip(id:int,updated_trip:schemas.TripCreate,db:Session = Depends(get_
     trip =trip_query.first()
     if trip == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Trip with id: {id} does not exist")
+                            detail=f"Trip with this car id: {id} does not exist")
    
     trip_query.update(updated_trip.dict(),synchronize_session = False)
     db.commit()
