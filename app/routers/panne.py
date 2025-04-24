a1e77ddae99aa18dadd6b1816fad189b6bfee47f
elvispronda <elvisndayishimiye200@gmail.com>
2025-04-23 18:38:13 -0700

    Auto Commit - 2025-04-23 18:38:12

--- app/routers/panne.py
+++ app/routers/panne.py
@@ -56,17 +56,17 @@ def delete_panne(id:int,db:Session = Depends(get_db), current_user : str = Depen
    return Response(status_code=status.HTTP_204_NO_CONTENT)
 ############################################################################################################################
 
-@router.put("/{id}", response_model=schemas.UserCreate)
-def update_user(id:int,updated_user:schemas.UserCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.PanneCreate)
+def update_panne(id:int,updated_panne:schemas.PanneCreate,db:Session = Depends(get_db), current_user : str = Depends(oauth2.get_current_user)):
     
   
-    user_query = db.query(models.User).filter(models.User.id == id)
-    user =user_query.first()
-    if user == None:
+    panne_query = db.query(models.Panne).filter(models.Panne.id == id)
+    panne=panne_query.first()
+    if panne == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id: {id} does not exist")
    
-    user_query.update(updated_user.dict(),synchronize_session = False)
+    panne_query.update(updated_panne.dict(),synchronize_session = False)
     db.commit()
-    return user_query.first()  
+    return panne_query.first()  
 ############################################################################################################################
